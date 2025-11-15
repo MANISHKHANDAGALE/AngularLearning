c102fbbe0efad9d42aa4995c83892ce1d031b22b
MANISH <khandagalemanish21@gmail.com>
2025-11-15 23:14:53 +0530

    pipes done

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CurrencyConverterPipe } from '../pipe/currency-converter.pipe';
 
 @Component({
   selector: 'app-pipes',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,CurrencyConverterPipe],
   templateUrl: './pipes.component.html',
   styleUrl: './pipes.component.css'
 })
 export class PipesComponent {
-
+amount=10
+title="angular learning"
+date= new Date()
 }
